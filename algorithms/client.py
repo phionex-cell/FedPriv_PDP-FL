dbd0304c6204587e23f9391f06e3cc4d70b97f69
phionex-cell <85338579+phionex-cell@users.noreply.github.com>
2025-07-29 10:51:35 +0800

    Add files via upload

---
+++ algorithms/client.py
@@ -0,0 +1,332 @@
+import copy
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from utils.utils import compute_accuracy
+
+def laplace_noise(sensitivity, epsilon, size):
+    return np.random.laplace(0, sensitivity / epsilon, size=size).astype(np.float32)
+
+def add_noise_to_model(net, epsilon):
+    for param in net.parameters():
+        if param.grad is not None:
+            # 生成NumPy噪声
+            noise = laplace_noise(1, epsilon, param.grad.shape)
+            # 转换为PyTorch张量并移动到对应设备
+            noise_tensor = torch.from_numpy(noise).to(param.grad.device)
+            # 添加噪声到梯度
+            param.grad += noise_tensor
+    return net
+
+def train_net(net_id, net, train_dataloader, test_dataloader, epochs, lr, args_optimizer, args, device="cpu", logger=None):
+    net.cuda()
+    logger.info('Training network %s' % str(net_id))
+
+    if args_optimizer == 'adam':
+        optimizer = optim.Adam(filter(lambda p: p.requires_grad, net.parameters()), lr=lr, weight_decay=args.reg)
+    elif args_optimizer == 'amsgrad':
+        optimizer = optim.Adam(filter(lambda p: p.requires_grad, net.parameters()), lr=lr, weight_decay=args.reg,
+                               amsgrad=True)
+    elif args_optimizer == 'sgd':
+        optimizer = optim.SGD(filter(lambda p: p.requires_grad, net.parameters()), lr=lr, momentum=0.9,
+                              weight_decay=args.reg)
+
+    criterion = nn.CrossEntropyLoss().cuda()
+    for epoch in range(epochs): 
+        epoch_loss_collector = []    
+        for batch_idx, (x, target) in enumerate(train_dataloader):
+            x, target = x.cuda(), target.cuda()
+            optimizer.zero_grad()
+            x.requires_grad = False
+            target.requires_grad = False
+            target = target.long()
+
+            _,_,out = net(x)
+            loss = criterion(out, target)
+            
+            loss.backward()
+            optimizer.step()
+            epoch_loss_collector.append(loss.item())
+        epoch_loss = sum(epoch_loss_collector) / len(epoch_loss_collector)
+        logger.info('Epoch: %d Loss: %f' % (epoch, epoch_loss))
+
+    test_acc, conf_matrix, _ = compute_accuracy(net, test_dataloader, get_confusion_matrix=True, device=device)
+    logger.info('>> Test accuracy: %f' % test_acc)
+    net.to('cpu')
+    return test_acc
+
+def train_net_scaffold(net_id, net, global_model, c_local, c_global, train_dataloader, test_dataloader, epochs, lr, args_optimizer, args, device="cpu", logger=None):
+    logger.info('Training network %s' % str(net_id))
+
+    if args_optimizer == 'adam':
+        optimizer = optim.Adam(filter(lambda p: p.requires_grad, net.parameters()), lr=lr, weight_decay=args.reg)
+    elif args_optimizer == 'amsgrad':
+        optimizer = optim.Adam(filter(lambda p: p.requires_grad, net.parameters()), lr=lr, weight_decay=args.reg,
+                               amsgrad=True)
+    elif args_optimizer == 'sgd':
+        optimizer = optim.SGD(filter(lambda p: p.requires_grad, net.parameters()), lr=lr, momentum=0.9, weight_decay=args.reg)
+    criterion = nn.CrossEntropyLoss().cuda()
+
+    cnt = 0
+    c_global_para = c_global.state_dict()
+    c_local_para = c_local.state_dict()
+
+    for epoch in range(epochs):
+        epoch_loss_collector = []
+        for batch_idx, (x, target) in enumerate(train_dataloader):
+            x, target = x.cuda(), target.cuda()
+            optimizer.zero_grad()
+            x.requires_grad = True
+            target.requires_grad = False
+            target = target.long()
+
+            _,_,out = net(x)
+            loss = criterion(out, target)
+
+            loss.backward()
+            optimizer.step()
+
+            net_para = net.state_dict()
+            for key in net_para:
+                net_para[key] = net_para[key] - args.lr * (c_global_para[key] - c_local_para[key])
+            net.load_state_dict(net_para)
+            cnt += 1
+            epoch_loss_collector.append(loss.item())
+
+        epoch_loss = sum(epoch_loss_collector) / len(epoch_loss_collector)
+        logger.info('Epoch: %d Loss: %f' % (epoch, epoch_loss))
+
+    c_new_para = c_local.state_dict()
+    c_delta_para = copy.deepcopy(c_local.state_dict())
+    global_model_para = global_model.state_dict()
+    net_para = net.state_dict()
+    for key in net_para:
+        c_new_para[key] = c_new_para[key] - c_global_para[key] + (global_model_para[key] - net_para[key]) / (cnt * args.lr)
+        c_delta_para[key] = c_new_para[key] - c_local_para[key]
+    c_local.load_state_dict(c_new_para)
+
+    test_acc, conf_matrix, _ = compute_accuracy(net, test_dataloader, get_confusion_matrix=True, device=device)
+    logger.info('>> Test accuracy: %f' % test_acc)
+    return test_acc, c_delta_para
+
+def train_net_fedprox(net_id, net, global_net, train_dataloader, test_dataloader, epochs, lr, args_optimizer, mu, args,
+                      device="cpu", logger=None):
+    logger.info('Training network %s' % str(net_id))
+    net.cuda()
+    if args_optimizer == 'adam':
+        optimizer = optim.Adam(filter(lambda p: p.requires_grad, net.parameters()), lr=lr, weight_decay=args.reg)
+    elif args_optimizer == 'amsgrad':
+        optimizer = optim.Adam(filter(lambda p: p.requires_grad, net.parameters()), lr=lr, weight_decay=args.reg,
+                               amsgrad=True)
+    elif args_optimizer == 'sgd':
+        optimizer = optim.SGD(filter(lambda p: p.requires_grad, net.parameters()), lr=lr, momentum=0.9,
+                              weight_decay=args.reg)
+
+    criterion = nn.CrossEntropyLoss().cuda()
+
+    global_weight_collector = list(global_net.cuda().parameters())
+
+    for epoch in range(epochs):
+        epoch_loss_collector = []
+        for batch_idx, (x, target) in enumerate(train_dataloader):
+            x, target = x.cuda(), target.cuda()
+
+            optimizer.zero_grad()
+            x.requires_grad = False
+            target.requires_grad = False
+            target = target.long()
+
+            _,_,out = net(x)
+            loss = criterion(out, target)
+
+            # For fedprox
+            fed_prox_reg = 0.0
+            for param_index, param in enumerate(net.parameters()):
+                fed_prox_reg += ((mu / 2) * torch.norm((param - global_weight_collector[param_index])) ** 2)
+            loss += fed_prox_reg
+
+            loss.backward()
+            optimizer.step()
+
+            epoch_loss_collector.append(loss.item())
+        epoch_loss = sum(epoch_loss_collector) / len(epoch_loss_collector)
+        logger.info('Epoch: %d Loss: %f' % (epoch, epoch_loss))
+
+    test_acc, conf_matrix, _ = compute_accuracy(net, test_dataloader, get_confusion_matrix=True, device=device)
+    net.to('cpu')
+    return test_acc
+
+def train_net_fedcon(net_id, net, global_net, previous_nets, train_dataloader, test_dataloader, epochs, lr, args_optimizer, mu, temperature, args,
+                      round, device="cpu", logger=None):
+    logger.info('Training network %s' % str(net_id))
+    net.cuda()
+
+    if args_optimizer == 'adam':
+        optimizer = optim.Adam(filter(lambda p: p.requires_grad, net.parameters()), lr=lr, weight_decay=args.reg)
+    elif args_optimizer == 'amsgrad':
+        optimizer = optim.Adam(filter(lambda p: p.requires_grad, net.parameters()), lr=lr, weight_decay=args.reg,
+                               amsgrad=True)
+    elif args_optimizer == 'sgd':
+        optimizer = optim.SGD(filter(lambda p: p.requires_grad, net.parameters()), lr=lr, momentum=0.9,
+                              weight_decay=args.reg)
+
+    criterion = nn.CrossEntropyLoss().cuda()
+
+    for previous_net in previous_nets:
+        previous_net.cuda()
+    cos=torch.nn.CosineSimilarity(dim=-1)
+
+    for epoch in range(epochs):
+        epoch_loss_collector = []
+        for batch_idx, (x, target) in enumerate(train_dataloader):
+            x, target = x.cuda(), target.cuda()
+
+            optimizer.zero_grad()
+            x.requires_grad = False
+            target.requires_grad = False
+            target = target.long()
+
+            _, pro1, out = net(x)
+            _, pro2, _ = global_net(x)
+
+            posi = cos(pro1, pro2)
+            logits = posi.reshape(-1,1)
+
+            for previous_net in previous_nets:
+                _, pro3, _ = previous_net(x)
+                nega = cos(pro1, pro3)
+                logits = torch.cat((logits, nega.reshape(-1,1)), dim=1)
+
+            logits /= temperature
+            labels = torch.zeros(x.size(0)).cuda().long()
+
+            loss2 = mu * criterion(logits, labels)
+            loss1 = criterion(out, target)
+            loss = loss1 + loss2
+            loss.backward()
+            optimizer.step()
+            epoch_loss_collector.append(loss.item())
+        epoch_loss = sum(epoch_loss_collector) / len(epoch_loss_collector)
+        logger.info('Epoch: %d Loss: %f' % (epoch, epoch_loss))
+    for previous_net in previous_nets:
+        previous_net.to('cpu')
+    test_acc, conf_matrix, _ = compute_accuracy(net, test_dataloader, get_confusion_matrix=True, device=device)
+    net.to('cpu')
+    return test_acc
+
+def local_train_net(epsilon_array, nets, args, net_dataidx_map, train_dl=None, test_dl=None, global_model = None, prev_model_pool = None, round=None, device="cpu", logger=None):
+
+    # 找出隐私阈值
+    t = max(epsilon_array)
+
+    acc_list = []
+    if global_model:
+        global_model.cuda()
+
+    # 选择这一轮参加训练的net
+    nets_this_round = {}
+    for net_id, net in nets.items():
+
+        if epsilon_array[net_id] <= 0:  # 如果 epsilon_array[net_id] <= 0，跳过当前 net
+            epsilon_array[net_id] = 0
+            continue
+
+        if epsilon_array[net_id] < t:
+            p = (np.exp(epsilon_array[net_id]) - 1) / (np.exp(t) - 1)
+        else:
+            p = 1
+
+        if np.random.rand() < p:  # 如果随机数小于 p，选择这个 net
+            nets_this_round[net_id] = net
+
+    for net_id, net in nets_this_round.items():
+        dataidxs = net_dataidx_map[net_id]
+        train_dl_local=train_dl[net_id]   
+        n_epoch = args.epochs
+        if args.alg == 'fedavg' or args.alg == 'fednova':
+            testacc = train_net(net_id, net, train_dl_local, test_dl, n_epoch, args.lr, args.optimizer, args,
+                                        device=device, logger=logger)
+        elif args.alg == 'fedprox':
+            testacc = train_net_fedprox(net_id, net, global_model, train_dl_local, test_dl, n_epoch, args.lr,
+                                                  args.optimizer, args.mu, args, device=device, logger=logger)
+        elif args.alg == 'moon':
+            prev_models=[]
+            for i in range(len(prev_model_pool)):
+                prev_models.append(prev_model_pool[i][net_id])
+            testacc = train_net_fedcon(net_id, net, global_model, prev_models, train_dl_local, test_dl, n_epoch, args.lr,
+                                                  args.optimizer, args.mu, args.temperature, args, round, device=device, logger=logger)
+
+        #为模型添加噪声    
+        net = add_noise_to_model(net, epsilon_array[net_id])
+        epsilon_array[net_id] = epsilon_array[net_id] - t / 10
+
+        print("Training network %s, n_training: %d, final test acc %f." % (str(net_id), len(dataidxs), testacc))
+
+        acc_list.append(testacc)
+    if global_model:
+        global_model.to('cpu')
+
+    print("Privacy budget after this round: ", epsilon_array)
+    return epsilon_array, acc_list
+
+def local_train_net_scaffold(epsilon_array, nets, global_model, c_nets, c_global, args, net_dataidx_map, train_dl=None, test_dl = None, device="cpu", logger=None):
+    total_delta = copy.deepcopy(global_model.state_dict())
+    for key in total_delta:
+        total_delta[key] = 0.0
+    c_global.cuda()
+    global_model.cuda()
+
+    # 找出隐私阈值
+    t = max(epsilon_array)
+
+    # 选择这一轮参加训练的net
+    nets_this_round = {}
+    for net_id, net in nets.items():
+
+        if epsilon_array[net_id] <= 0:  # 如果 epsilon_array[net_id] <= 0，跳过当前 net
+            epsilon_array[net_id] = 0
+            continue
+
+        if epsilon_array[net_id] < t:
+            p = (np.exp(epsilon_array[net_id]) - 1) / (np.exp(t) - 1)
+        else:
+            p = 1
+
+        if np.random.rand() < p:  # 如果随机数小于 p，选择这个 net
+            nets_this_round[net_id] = net
+
+
+
+    for idx, (net_id, net) in enumerate(nets_this_round.items()):
+        dataidxs = net_dataidx_map[net_id]
+        train_dl_local = train_dl[net_id]
+        net.cuda()
+        c_nets[net_id].cuda()
+        n_epoch = args.epochs
+
+        testacc, c_delta_para = train_net_scaffold(net_id, net, global_model, c_nets[net_id], c_global, train_dl_local, test_dl, n_epoch, args.lr, args.optimizer, args, device=device, logger=logger)
+
+        # 为模型添加噪声
+        net = add_noise_to_model(net, epsilon_array[net_id])
+        epsilon_array[net_id] = epsilon_array[net_id] - t / 10
+
+        c_nets[net_id].to('cpu')
+        for key in total_delta:   
+            total_delta[key] += c_delta_para[key] / len(c_nets)
+        print("Training network %s, n_training: %d, final test acc %f." % (str(net_id), len(dataidxs), testacc))
+
+    c_global_para = c_global.state_dict()
+    for key in c_global_para:
+        if c_global_para[key].type() == 'torch.LongTensor':
+            c_global_para[key] += total_delta[key].type(torch.LongTensor)
+        elif c_global_para[key].type() == 'torch.cuda.LongTensor':
+            c_global_para[key] += total_delta[key].type(torch.cuda.LongTensor)
+        else:
+            c_global_para[key] += total_delta[key]
+    c_global.load_state_dict(c_global_para)
+
+    nets_list = list(nets.values())
+
+    print("Privacy budget after this round: ", epsilon_array)
+    return epsilon_array, nets_list
---
+++ algorithms/feddyn.py
@@ -0,0 +1,164 @@
+import copy
+import torch
+import numpy as np
+import torch.optim as optim
+from utils.model import *
+from utils.utils import *
+from disco import *
+
+def laplace_noise(sensitivity, epsilon, size):
+    return np.random.laplace(0, sensitivity / epsilon, size=size).astype(np.float32)
+
+def add_noise_to_model(net, epsilon):
+    for param in net.parameters():
+        if param.grad is not None:
+            # 生成NumPy噪声
+            noise = laplace_noise(1, epsilon, param.grad.shape)
+            # 转换为PyTorch张量并移动到对应设备
+            noise_tensor = torch.from_numpy(noise).to(param.grad.device)
+            # 添加噪声到梯度
+            param.grad += noise_tensor
+    return net
+
+def feddyn_alg(args, epsilon_array, n_comm_rounds, global_model, party_list_rounds, net_dataidx_map, train_local_dls, test_dl, traindata_cls_counts, device, global_dist, logger):
+    best_test_acc=0
+    record_test_acc_list = []
+
+    alpha_coef = 1e-2
+    n_par = len(get_mdl_params([global_model])[0])  
+    local_param_list = np.zeros((args.n_parties, n_par)).astype('float32')  
+    init_par_list=get_mdl_params([global_model], n_par)[0]
+    clnt_params_list  = np.ones(args.n_parties).astype('float32').reshape(-1, 1) * init_par_list.reshape(1, -1)
+    avg_model = copy.deepcopy(global_model)
+    all_model = copy.deepcopy(global_model)
+    cld_model = copy.deepcopy(global_model)
+    cld_mdl_param = get_mdl_params([cld_model], n_par)[0]
+    weight_list = np.asarray([len(net_dataidx_map[i]) for i in range(args.n_parties)])
+    weight_list = weight_list / np.sum(weight_list) * args.n_parties
+    for round in range(n_comm_rounds):
+        logger.info("in comm round:" + str(round))
+        print("In communication round:" + str(round))
+        party_list_this_round = party_list_rounds[round]
+        if args.sample_fraction<1.0:
+            print(f'Clients this round : {party_list_this_round}')
+        cld_mdl_param_tensor = torch.tensor(cld_mdl_param, dtype=torch.float32).cuda()
+
+        # 找出隐私阈值
+        t = max(epsilon_array)
+
+        # 选择这一轮参加训练的net
+        party_list_this_round = []
+        for i in range(10):
+
+            if epsilon_array[i] <= 0:  # 如果 epsilon_array[i] <= 0，跳过当前 net
+                epsilon_array[i] = 0
+                continue
+
+            if epsilon_array[i] < t:
+                p = (np.exp(epsilon_array[i]) - 1) / (np.exp(t) - 1)
+            else:
+                p = 1
+
+            if np.random.rand() < p:  # 如果随机数小于 p，选择这个 net
+                party_list_this_round.append(i)
+
+
+        for clnt in party_list_this_round:
+            train_dataloader=train_local_dls[clnt]
+            model = copy.deepcopy(global_model).cuda()
+            # Warm start from current avg model
+            model.load_state_dict(cld_model.state_dict())
+            for params in model.parameters():
+                params.requires_grad = True
+            # Scale down
+            alpha_coef_adpt = alpha_coef / weight_list[clnt] # adaptive alpha coef
+            local_param_list_curr = torch.tensor(local_param_list[clnt], dtype=torch.float32, device='cuda')
+            loss_fn = torch.nn.CrossEntropyLoss(reduction='sum')
+            optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=0.9, weight_decay=alpha_coef+args.reg)
+            model.train()
+            model.cuda()
+            for e in range(args.epochs):
+                # Training
+                for batch_idx, (batch_x, batch_y) in enumerate(train_dataloader):
+                    batch_x = batch_x.cuda()
+                    batch_y = batch_y.cuda()
+                    
+                    batch_x.requires_grad=False
+                    batch_y.requires_grad=False
+                    
+                    optimizer.zero_grad()
+                    _,_,y_pred = model(batch_x)
+                    
+                    ## Get f_i estimate 
+                    loss_f_i = loss_fn(y_pred, batch_y.reshape(-1).long())
+                    loss_f_i = loss_f_i / list(batch_y.size())[0]
+                    
+                    # Get linear penalty on the current parameter estimates
+                    local_par_list = None
+                    for param in model.parameters():
+                        if not isinstance(local_par_list, torch.Tensor):
+                        # Initially nothing to concatenate
+                            local_par_list = param.reshape(-1)
+                        else:
+                            local_par_list = torch.cat((local_par_list, param.reshape(-1)), 0)
+                    loss_algo = alpha_coef_adpt * torch.sum(local_par_list * (-cld_mdl_param_tensor + local_param_list_curr))
+                    loss = loss_f_i + loss_algo
+                    loss.backward()
+                    torch.nn.utils.clip_grad_norm_(parameters=model.parameters(), max_norm=10) # Clip gradients
+                    optimizer.step()
+            # Freeze model
+            for params in model.parameters():
+                params.requires_grad = False
+            model.eval()
+            curr_model_par = get_mdl_params([model], n_par)[0]
+            # No need to scale up hist terms. They are -\nabla/alpha and alpha is already scaled.
+            local_param_list[clnt] += curr_model_par-cld_mdl_param
+            clnt_params_list[clnt] = curr_model_par
+            model.cuda()
+            test_acc, conf_matrix, _ = compute_accuracy(model, test_dl, get_confusion_matrix=True, device=device)
+
+            # 为每个模型添加噪声
+            model = add_noise_to_model(model, epsilon_array[clnt])
+            epsilon_array[clnt] = epsilon_array[clnt] - t / 10
+
+            print("Training network %s, n_training: %d, final test acc %f." % (str(clnt), len(train_dataloader.dataset), test_acc))
+            model.to('cpu')
+
+        print("Privacy budget after this round: ", epsilon_array)
+        # Aggregation weight calculation
+        total_data_points = sum([len(net_dataidx_map[r]) for r in party_list_this_round])
+        fed_avg_freqs = [len(net_dataidx_map[r]) / total_data_points for r in party_list_this_round]
+        if round==0 or args.sample_fraction<1.0:
+            print(f'Dataset size weight : {fed_avg_freqs}')
+        if args.disco: # Discrepancy-aware collaboration
+            distribution_difference = get_distribution_difference(traindata_cls_counts, participation_clients=party_list_this_round, metric=args.measure_difference, hypo_distribution=global_dist)
+
+            selected_epsilons = epsilon_array[party_list_this_round]  # 筛选参与客户端的隐私预算
+            fed_avg_freqs = disco_weight_adjusting(selected_epsilons, fed_avg_freqs, distribution_difference,args.disco_a, args.disco_b)
+
+            if round==0 or args.sample_fraction<1.0:
+                print(f'Distribution_difference : {distribution_difference}\nDisco Aggregation Weights : {fed_avg_freqs}') 
+            avg_mdl_param = np.dot(np.array(fed_avg_freqs).reshape(1,-1), clnt_params_list[party_list_this_round])
+            avg_mdl_param = np.squeeze(avg_mdl_param)
+        else: # Model aggregation
+            avg_mdl_param = np.mean(clnt_params_list[party_list_this_round], axis = 0)
+        cld_mdl_param = avg_mdl_param + np.mean(local_param_list, axis=0)
+        avg_model = set_client_from_params(copy.deepcopy(global_model), avg_mdl_param)
+        all_model = set_client_from_params(copy.deepcopy(global_model), np.mean(clnt_params_list, axis = 0))
+        cld_model = set_client_from_params(copy.deepcopy(global_model), cld_mdl_param) 
+        avg_model.cuda()
+
+        # Test            
+        test_acc, conf_matrix, _= compute_accuracy(avg_model, test_dl, get_confusion_matrix=True, device=device)
+        record_test_acc_list.append(test_acc)
+        if(best_test_acc<test_acc):
+            best_test_acc=test_acc
+            logger.info('New best test acc:%f'% test_acc)
+        logger.info('>> Global Model Test accuracy: %f' % test_acc)
+        logger.info('>> Global Model Best accuracy: %f' % best_test_acc)
+        print('>> Global Model Test accuracy: %f, Best: %f' % (test_acc, best_test_acc))
+        avg_model.to('cpu')
+        mkdirs(args.modeldir+'feddyn/')
+        if args.save_model:
+            torch.save(global_model.state_dict(), args.modeldir+'feddyn/'+'globalmodel'+args.log_file_name+'.pth')
+    return record_test_acc_list, best_test_acc
